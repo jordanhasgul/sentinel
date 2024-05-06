5a7e196834cd374b7f45410c77bc1beb9ec03689
Jordan Hasgul <jordan.hasgul@transferwise.com>
2024-05-06 23:21:01 +0100

    feat: implement a transformation validator
    Signed-off-by: Jordan Hasgul <jordan.hasgul@transferwise.com>

--- sentinel.go
+++ sentinel.go
@@ -1,37 +1,42 @@
 package sentinel
 
-import "github.com/jordanhasgul/multierr"
+import (
+	"github.com/jordanhasgul/multierr"
+)
 
-// Validator validates instances of a type, T.
+// Validator validates instances of type T.
 type Validator[T any] interface {
 	Validate(t T) (bool, error)
 }
 
-// ValidateFunc is an adapter that allows the use of ordinary
-// Go functions as validators. If f is a function with the appropriate
-// signature, then ValidateFunc(f) is a [Validator] that calls f.
+// ValidateFunc is an adapter that allows the use of ordinary Go functions
+// as validators. If f is a function with the appropriate signature, then
+// ValidateFunc(f) is a Validator that calls f.
 type ValidateFunc[T any] func(t T) (bool, error)
 
+// Validate calls f(t).
 func (f ValidateFunc[T]) Validate(t T) (bool, error) {
 	return f(t)
 }
 
-// Valid returns a Validator that always returns true.
+// Valid returns a Validator that always returns true when validating
+// instances of type T.
 func Valid[T any]() Validator[T] {
 	return ValidateFunc[T](func(t T) (bool, error) {
 		return true, nil
 	})
 }
 
-// Invalid returns a [Validator] that always returns false.
+// Invalid returns a Validator that always returns false when validating
+// instances of T.
 func Invalid[T any]() Validator[T] {
 	return ValidateFunc[T](func(t T) (bool, error) {
 		return false, nil
 	})
 }
 
-// Not takes a single [Validator] and returns a [Validator] that negates
-// the result of the original validator.
+// Not returns a Validator that negates the result of v when validating
+// instances of T.
 func Not[T any](v Validator[T]) Validator[T] {
 	return ValidateFunc[T](func(t T) (bool, error) {
 		ok, err := v.Validate(t)
@@ -43,8 +48,8 @@ func Not[T any](v Validator[T]) Validator[T] {
 	})
 }
 
-// And takes multiple [Validator] and returns a [Validator] that returns true
-// if all the provided validators succeed.
+// And returns a Validator that returns true if all the validators in vs
+// return true when validating instances of T.
 func And[T any](vs ...Validator[T]) Validator[T] {
 	if len(vs) == 0 {
 		return Valid[T]()
@@ -66,8 +71,8 @@ func And[T any](vs ...Validator[T]) Validator[T] {
 	})
 }
 
-// Or takes multiple [Validator] and returns a [Validator] that returns true
-// if any of the provided validators succeed.
+// Or returns a Validator that returns true if any of the validators in vs
+// return true when validating instances of T.
 func Or[T any](vs ...Validator[T]) Validator[T] {
 	if len(vs) == 0 {
 		return Valid[T]()
@@ -86,5 +91,13 @@ func Or[T any](vs ...Validator[T]) Validator[T] {
 		}
 
 		return true, nil
+	})
+}
+
+// WithValue returns a Validator that returns true if v returns true when
+// validating instances of T under the application of f.
+func WithValue[T, U any](f func(T) U, v Validator[U]) Validator[T] {
+	return ValidateFunc[T](func(t T) (bool, error) {
+		return v.Validate(f(t))
 	})
 }
